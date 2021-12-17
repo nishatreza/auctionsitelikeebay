884de3e8422478d3657b8f786f72201a4d84730a
M. Mobasher Reza <30533488+nishatreza@users.noreply.github.com>
2021-12-17 23:07:04 +0600

    2nd
    2nd commit

--- aunctionSiteDjango/auctionebay/settings.py
+++ aunctionSiteDjango/auctionebay/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'accounts'
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'auctionebay.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'auctionebay.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'auctionebayDB',
+        'USER': 'postgres',
+        'PASSWORD': '111111',
+        'HOST': 'localhost',
+
     }
 }
 
@@ -116,6 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ aunctionSiteDjango/home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, "index.html", {'products': products})
